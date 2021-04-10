486285a576aad6d5c1d1441b4e958ae14e5e3634
maxwell.wayne.17 <maxwell.wayne.17@cnu.edu>
2021-04-09 21:42:38 -0400

    complete project push

--- src/410_proj_thread_log.cpp
+++ src/410_proj_thread_log.cpp
@@ -15,30 +15,54 @@ using namespace std;
 
 //NOTE: you should have no mutexes in this file
 //TODO linker errors?  Did you include the pthread library?   And set the proper dialect?
-//TODO declare globals
+//TODO declare globals, one for file one for console
+Logger fileLogger(LOG_FILE);
+Logger consoleLogger(LOG_CONSOLE);
+// Boolean to stop doing work once program is done sleeping
+bool doWork = true;
 
 /***
  * TODO log info to both file and console. You can do this with 2 Logger objects. 
  * 		Note:  These logger objects cannot be local variables, they must be shared 
- * 		amongst the threads. (why?)
+ * 		amongst the threads. (why?) - Otherwise there will be no concurrency
  * TODO this function should run until main (or another thread) asks it to quit
  * @param info  to log
  * returns void
  */
 void fun(string info){
 
+	// Allows main to ask threads to stop doing work
+	if (doWork){
+		fileLogger.Log(info);
+		consoleLogger.Log(info);
+	}
+
+
 }
 int main() {
 	
 	//TODO start as many threads as you have cores (see std::thread::hardware_concurrency())
 	//TODO save these threads in a vector
+	std::vector<std::thread> vecThreads;
+	int numThreads = std::thread::hardware_concurrency();
+
+	std::string input = "a";
+	for (int i=0; i < numThreads/2; i++){
+		//Simple way to differentiate the input string for the threads
+		input.append("a");
+		vecThreads.push_back( std::thread( fun, input ) );
+	}
 
 	//TODO let threads run a bit (5 seconds)
 	this_thread::sleep_for(chrono::milliseconds(5000));
 	
 	//TODO ask them all to stop
-	
+	doWork = false;
+
 	//TODO wait for all threads to finish
+	for (auto& t : vecThreads){
+		t.join();
+	}
 	
 	return 0;
 }
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -6,4 +6,40 @@
  */
 #include <iostream>
 #include "../includes/Logger.h"
+#include "../includes/constants.h"
 using namespace std;
+
+Logger::Logger(LOG_TYPE lt, string fn){
+	Logger::lt = lt;
+	Logger::fn = fn;
+	//open file if lt is for files
+	if (lt == LOG_FILE){
+		Logger::fs = ofstream(fn);
+	}
+}
+
+void Logger::Log(string info){
+
+	if (lt == LOG_CONSOLE){
+		mtx.lock();
+		cout << info << endl;
+		mtx.unlock();
+	}
+	else{ //LOG_FILE
+		mtx.lock();
+		fs << info << endl;
+		mtx.unlock();
+	}
+
+
+}
+
+Logger::~Logger(){
+	//Unlock just in case
+	mtx.unlock();
+	// Close file
+	if ( fs.is_open() ){
+		fs.close();
+	}
+}
+
